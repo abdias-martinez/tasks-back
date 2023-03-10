669ef1fd41276ca40cbb27a050253e45815f00da
Abdias mb <amartinez@tinkin.one>
2023-03-10 15:36:28 -0500

    fix: corrección de observaciones

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,25 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { Document } from 'mongoose'
 import { TypeStatusEnum } from '../interfaces/task-status'
-
+import { ERROR_MESSAGES } from '../../constants/errores'
 @Schema({ timestamps: true })
 export class Task extends Document {
   @Prop({
     index: true,
-    required: true,
+    required: [true, ERROR_MESSAGES.IsNotEmpty],
   })
   taskName: string
 
   @Prop({
     index: true,
-    required: true,
+    required: [true, ERROR_MESSAGES.IsNotEmpty],
   })
   taskDescription: string
 
   @Prop({
     unique: true,
     index: true,
-    required: true,
+    required: [true, ERROR_MESSAGES.IsNotEmpty],
   })
   code: string
 
